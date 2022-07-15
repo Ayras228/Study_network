f53c473447f2470c76beeff6ccbc703d2c2b76f7
Sotnikov Maksym <80080035@knu.ua>
2022-07-15 17:05:41 +0300

    update

--- SFML Network/main.cpp
+++ SFML Network/main.cpp
@@ -2,17 +2,17 @@
 #include<iostream>
 #include<string>
 #include<Windows.h>
-
-
-
+#include <chrono>
+#include <thread>
 
 
 int main()
 {
 	sf::TcpSocket socket;
+	//sf::IpAddress ip = "192.168.3.6";//I
 	//sf::IpAddress ip = "192.168.3.8";notebook
-	sf::IpAddress ip = "192.168.24.248";//aleksey
-	//sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	//sf::IpAddress ip = "192.168.24.248";//aleksey
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 	std::cout << "local: " << ip << "\n";
 	std::cout <<"public: " << sf::IpAddress::getPublicAddress() << "\n";
 	char type;
@@ -22,8 +22,8 @@ int main()
 	if (type=='s')
 	{
 		sf::TcpListener listner;
-		listner.listen(2000);
-		name = "server\n";
+		listner.listen(5000);
+		name = "server";
 		if (listner.accept(socket) != sf::Socket::Done)
 		{
 			std::cout << "Error!\n";
@@ -32,33 +32,45 @@ int main()
 	else if (type == 'c')
 	{
 		name = "client ";
-		if (socket.connect(ip, 2000) != sf::Socket::Done)
+		std::cout << "connect to ip" << std::endl;
+		std::cin >> ip; 
+		if (socket.connect(ip, 5000) != sf::Socket::Done)
 		{
 			std::cout << "Error!\n";
 		}
+		
 	}
+	else
+	{
+		std::cout << "no connection \t error symbol\n";
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+		std::exit(1);
+	}
+
+	std::cout << "connect \n";
 	//std::string name;
 	//std::cout << "Enter your name \n";
 	//std::cin >> name;
 	socket.setBlocking(false);
 	std::string message = "";
 	sf::Packet packet;
-	bool stop = true;
+	std::cout << "Enter message\n";
 	while (true)
 	{
-		if (GetAsyncKeyState(VK_ESCAPE))
-		{
-			std::cin >> message;
-		}
+
+		std::cin >> message;
+		
 		if (message!="")
 		{
 			packet.clear();
 			packet << name << message;
 			socket.send(packet);
 			message = "";
+			//socket.setBlocking(true);
 		}
 		if (socket.receive(packet)==sf::Socket::Done)
 		{
+			//socket.setBlocking(false);
 			std::string nameRec;
 			std::string messageRec;
 
